8d171fa14bc0ae4fd8b6f25a4acbc0628c4e1c63
Kapil Verma <kapv89@gmail.com>
2016-08-07 11:47:49 +0530

    onback ready

---
+++ readme.md
@@ -0,0 +1,99 @@
+# react-native-onback
+
+## Handle complex back button flows on android
+
+#### Usage:
+
+- In **index.android.js**
+```js
+const React = require('react');
+
+const onBack = require('react-native-onback');
+
+class App extends React.Component {
+  componentDidMount() {
+    // start handling backButton via onBack
+    onBack.hook();
+  }
+
+  componentWillUnmount() {
+    // stop handling backButton via onBack
+    onBack.unhook();
+  }
+
+  render() {
+    return (
+      ...
+    );
+  }
+}
+```
+
+- Inside a **component** with a `Navigator`
+```js
+const React = require('react');
+
+const onBack = require('react-native-onback');
+
+class SomeScreen extends React.Component {
+  componentDidMount() {
+    onBack(() => {
+      // onBack maintains a stack of listeners.
+      // This listener will be at the bottom of the stack and
+      // will be called if no other listeners registered after
+      // it stop the propagation of back event
+      BackAndroid.exitApp();
+    }, this); // <----- NOTICE THE `this`. This lets onBack know the context of the listener
+
+    onBack((ev) => {
+      // This listener is one level above the previous listener,
+      // as it is registered after it. It can call ev.stopPropagation
+      // on the backButton event to stop any listeners registered before it
+      // from responding to backButton
+      if (this.refs.navigator.getCurrentRoutes().length > 1) {
+        requestAnimationFrame(() => this.refs.navigator.pop());
+        // this is how you stop the propagation of back button event
+        // at a particular listener
+        ev.stopPropagation()
+      }
+    }, this);
+  }
+
+  componentWillUnmount() {
+    // removes all listeners registered inside this particular component
+    onBack.unmount(this);
+  }
+
+  render() {
+    return (
+      ...
+    );
+  }
+}
+```
+
+
+- Inside __any__ other random **Component** loaded via `navigator`
+```js
+class SomeOtherComponent extends React.Component {
+  onSomeUserActionRegisterOnBackListener() {
+    onBack((ev) => {
+      if (this.state.someCondition) {
+        this.doSomethingOnBack();
+        ev.stopPropagation();
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    onBack.unmount(this);
+  }
+
+  render() {
+    return (
+      ...
+    );
+  }
+}
+
+```
---
+++ src/index.js
@@ -0,0 +1,120 @@
+const {BackAndroid} = require('react-native');
+const {isFunction} = require('lodash');
+/*
+onBack((ev) => {...}, this);
+onBack.unmount(this);
+
+onBack((ev) => {...}, this);
+*/
+
+class BackButtonHandler {
+  constructor() {
+    this.handlers = new Map();
+    this.contexts = new Set();
+
+    this.handleBack = () => this.onBack();
+  }
+
+  onBack() {
+    const ev = new BackEvent();
+    const contexts = Array.from(this.contexts).reverse();
+
+    for (const i in contexts) {
+      const ctx = contexts[i];
+      const handlers = Array.from(this.handlers.get(ctx)).reverse();
+      for (const i in handlers) {
+        const handler = handlers[i];
+        handler.bind(ctx)(ev);
+
+        if (!ev.propagate) {
+          return true;
+        }
+      }
+    }
+
+    return true;
+  }
+
+  mount(handler=(()=>{}), ctx) {
+    if (!this.handlers.has(ctx)) {
+      this.handlers.set(ctx, new Set());
+    }
+
+    if (!this.contexts.has(ctx)) {
+      this.contexts.add(ctx);
+    }
+
+    if (!this.handlers.get(ctx).has(handler)) {
+      this.handlers.get(ctx).add(handler);
+    }
+
+    return this;
+  }
+
+  unmount(...args) {
+    switch (args.length) {
+      case 0: throw new Error('invalid args');
+      case 1: return this.unmountContext(args[0]);
+      default: return this.unmountHandler(args[0], args[1]);
+    }
+  }
+
+  unmountContext(ctx) {
+    if (this.handlers.has(ctx) && this.contexts.has(ctx)) {
+      this.handlers.delete(ctx);
+      this.contexts.delete(ctx);
+    }
+
+    return this;
+  }
+
+  unmountHandler(handler, ctx) {
+    if (this.handlers.has(ctx) && this.handlers.get(ctx).has(handler)) {
+      this.handlers.get(ctx).delete(handler);
+
+      if (this.handlers.get(ctx).size === 0 && this.contexts.has(ctx)) {
+        this.handlers.delete(ctx);
+        this.contexts.delete(ctx);
+      }
+    }
+
+    return this;
+  }
+
+  hook() {
+    BackAndroid.addEventListener('hardwareBackPress', this.handleBack);
+  }
+
+  unhook() {
+    BackAndroid.removeEventListener('hardwareBackPress');
+  }
+}
+
+class BackEvent {
+  constructor() {
+    this.propagate = true;
+  }
+
+  stopPropagation() {
+    this.propagate = false;
+  }
+}
+
+const backButtonHandler = new BackButtonHandler();
+const defaultContext = {};
+
+function onBack(handler=(()=>{}), ctx=defaultContext) {
+  backButtonHandler.mount(handler, ctx);
+}
+
+onBack.unmount = (...args) => args.length === 1 && isFunction(args[0]) ?
+  backButtonHandler.unmount(args[0], defaultContext) :
+  backButtonHandler.unmount(...args)
+;
+
+onBack.hook = () => backButtonHandler.hook();
+onBack.unhook = () => backButtonHandler.unhook();
+
+onBack._handler = backButtonHandler;
+
+module.exports = onBack;
